a9c2c1a145872ad0337c4061850cb29e2c1a3a7a
Gerardo Espinosa <ivanmtze96@gmail.com>
2019-10-15 12:46:51 -0500

    Corrijo: cuando cerrabas la app, te sacaba y tenias que volver a loggearte

--- app/src/main/java/com/app/tinderuv/SplashScreen.java
+++ app/src/main/java/com/app/tinderuv/SplashScreen.java
@@ -5,14 +5,29 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashScreen extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class SplashScreen extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        auth = FirebaseAuth.getInstance();
+
+        checkUserStatus();
+    }
+
+    private void checkUserStatus(){
+        currentUser = auth.getCurrentUser();
+        if(currentUser!=null){
+            startActivity(new Intent(SplashScreen.this, ProfileActivity.class));
+            finish();
+        }else{
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        }
     }
 }
